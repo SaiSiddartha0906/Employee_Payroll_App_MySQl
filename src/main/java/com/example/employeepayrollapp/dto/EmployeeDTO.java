27008cdc755c51b2fd1dc5aebbce99add232a13f
SaiSiddartha0906 <saisiddartha09062004@gmail.com>
2025-05-24 15:53:10 +0530

    UC6:Added:Lombok Getter and Setters

--- src/main/java/com/example/employeepayrollapp/dto/EmployeeDTO.java
+++ src/main/java/com/example/employeepayrollapp/dto/EmployeeDTO.java
@@ -1,5 +1,8 @@
 package com.example.employeepayrollapp.dto;
-
+import lombok.*;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class EmployeeDTO {
 
     private Long id;
@@ -7,61 +10,4 @@ public class EmployeeDTO {
     private String department;
     private Double salary;
 
-    // Default constructor
-    public EmployeeDTO() {
-    }
-
-    // Parameterized constructor
-    public EmployeeDTO(Long id, String name, String department, Double salary) {
-        this.id = id;
-        this.name = name;
-        this.department = department;
-        this.salary = salary;
-    }
-
-    // Getter and Setter methods
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDepartment() {
-        return department;
-    }
-
-    public void setDepartment(String department) {
-        this.department = department;
-    }
-
-    public Double getSalary() {
-        return salary;
-    }
-
-    public void setSalary(Double salary) {
-        this.salary = salary;
-    }
-    
-
-    // Optional: toString()
-    @Override
-    public String toString() {
-        return "EmployeeDTO{" +
-               "id=" + id +
-               ", name='" + name + '\'' +
-               ", department='" + department + '\'' +
-               ", salary=" + salary +
-               '}';
-    }
 }
--- src/main/java/com/example/employeepayrollapp/model/Employee.java
+++ src/main/java/com/example/employeepayrollapp/model/Employee.java
@@ -5,8 +5,18 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class Employee {
 
     @Id
@@ -17,63 +27,5 @@ public class Employee {
     private String department;
     private Double salary;
 
-    // Default constructor
-    public Employee() {
-    }
-
-    // Parameterized constructor
-    public Employee(Long id, String name, String department, Double salary) {
-        this.id = id;
-        this.name = name;
-        this.department = department;
-        this.salary = salary;
-    }
-
-    // Getters
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getDepartment() {
-        return department;
-    }
-
-    public Double getSalary() {
-        return salary;
-    }
-
-    // Setters
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setDepartment(String department) {
-        this.department = department;
-    }
-
-    public void setSalary(Double salary) {
-        this.salary = salary;
-    }
-
-    // Optional: toString()
-
-    @Override
-    public String toString() {
-        return "Employee{" +
-               "id=" + id +
-               ", name='" + name + '\'' +
-               ", department='" + department + '\'' +
-               ", salary=" + salary +
-               '}';
-    }
+    
 }
